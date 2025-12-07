f3f30b0d299b3d794d01b05b19533a12b5f138ce
Praveen <praveenyennam@gmail.com>
2025-12-08 02:03:38 +0530

    Retried adding .env file in gitignore and saved the API Key inside .env file

--- video_stats.py
+++ video_stats.py
@@ -21,7 +21,7 @@ def get_playlist_id():
         channel_items = data["items"][0]
         channel_playlistId = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
 
-        print(channel_playlistId)
+        #print(channel_playlistId)
         return channel_playlistId
     except requests.exceptions.RequestException as e:
         raise e
